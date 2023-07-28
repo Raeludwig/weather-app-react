6403b2e750064a955803ffcb07314f8958fe0b9f
Raeludwig <racheleludwig@gmail.com>
2023-07-27 23:30:26 -0600

    background and font fixed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import React, {useState} from "react";
 import axios from "axios";
 
 function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=f4cc82078e0999601d888c5956e645ef`
+
+
   return (
-    <div className="App">
-   
+    <div className="app">
+  <div className="container">
+
+    <div className="top">
+      <div className="location">
+        <p>Dallas</p>
+      </div>
+      <div className="temp">
+        <h1>65 Degrees</h1>
+      </div>
+      <div className="desc">
+        <p>Clouds</p>
+      </div>
+    </div>
+
+<div className="bottom">
+  <div className="feels">
+    <p>65 Degrees</p>
+  </div>
+  <div className="humidity">
+    <p>20%</p>
+  </div>
+  <div className="wind">
+    12MPH
+  </div>
+</div>
+  </div>
     </div>
   );
 }
